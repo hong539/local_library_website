0d7380d409d1b569d38a67ece1f8fbe54534f050
hong@arch-pyton-dev <yunghunghuang984@gmail.com>
2023-08-26 08:52:55 +0800

    Test codes

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -90,10 +90,10 @@ class Author(models.Model):
     """Model representing an author."""
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)    
-    # year_of_birth = models.DateField(null=True, blank=True)
-    # year_of_death = models.DateField('Died', null=True, blank=True)
-    year_of_birth = models.IntegerField()
-    year_of_death = models.IntegerField()
+    year_of_birth = models.DateField(null=True, blank=True)
+    year_of_death = models.DateField('Died', null=True, blank=True)
+    # year_of_birth = models.IntegerField(null=True, blank=True)
+    # year_of_death = models.IntegerField(null=True, blank=True)
 
     class Meta:
         ordering = ['last_name', 'first_name']
