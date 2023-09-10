f446c38971439b4994369960a279ff4dc9f15b2b
hong@arch-pyton-dev <yunghunghuang984@gmail.com>
2023-09-10 09:30:43 +0800

    Update architecture

--- architecture/diagram.py
+++ architecture/diagram.py
@@ -1,24 +1,6 @@
-from diagrams import Cluster, Diagram
-from diagrams.aws.compute import ECS
-from diagrams.aws.database import ElastiCache, RDS
-from diagrams.aws.network import ELB
-from diagrams.aws.network import Route53
+from diagrams import Diagram
+from diagrams.onprem.compute import Server
+from diagrams.onprem.database import PostgreSQL
 
-with Diagram("Clustered Web Services", show=False):
-    dns = Route53("dns")
-    lb = ELB("lb")
-
-    with Cluster("Services"):
-        svc_group = [ECS("web1"),
-                     ECS("web2"),
-                     ECS("web3")]
-
-    with Cluster("DB Cluster"):
-        db_primary = RDS("userdb")
-        db_primary - [RDS("userdb ro")]
-
-    memcached = ElastiCache("memcached")
-
-    dns >> lb >> svc_group
-    svc_group >> db_primary
-    svc_group >> memcached
+with Diagram(name="local_library_website with On-Premise", show=False):    
+    Server("Django development server") >> PostgreSQL("main")
